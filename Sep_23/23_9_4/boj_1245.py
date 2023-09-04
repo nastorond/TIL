f3606803c4f865e418f7ac8dc22cb39d8ed708dc
nastorond <nastorond@kakao.com>
2023-09-05 07:26:04 +0900

    asd

---
+++ Sep_23/23_9_4/boj_1245.py
@@ -0,0 +1,31 @@
+import sys
+from collections import deque
+# input = sys.stdin.readline
+
+
+def bfs(x, y):
+    q = deque()
+    q.append([x, y])
+    visited[x][y] = 1
+    while q:
+        dx, dy = q.popleft()
+        for di, dj in [[0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1]]:
+            ni, nj = dx + di, dy + dj
+            if 0 <= ni < n and 0 <= nj < m and fld[ni][nj] and not visited[ni][nj]:
+                if fld[dx][dy] < fld[ni][nj]:
+                    return 0
+                q.append([ni, nj])
+                visited[ni][nj] = 1
+    return 1
+
+
+n, m = map(int, input().split())
+fld = [list(map(int, input().split())) for _ in range(n)]
+visited = [[0] * m for _ in range(n)]
+
+res = 0
+for i in range(n):
+    for j in range(m):
+        if not visited[i][j] and fld[i][j]:
+            res += bfs(i, j)
+print(res)
