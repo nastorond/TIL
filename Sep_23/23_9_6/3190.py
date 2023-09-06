23d83fed026300bf0a0feec81049e7416722bc0a
nas <nastorond@kakao.com>
2023-09-06 21:58:26 +0900

    add prob

---
+++ Sep_23/23_9_6/3190.py
@@ -0,0 +1,48 @@
+import sys
+from collections import deque
+
+
+def game():
+    q = deque()
+    q.append((0,0))
+    sec = 0
+    direction = 0
+    while 1:
+        sec += 1
+        x, y = dir[direction]
+        nx, ny = q[0]
+        dx, dy = x + nx, y + ny
+        if 0<=dx<n and 0<=dy<n:
+            if (dx, dy) in q:
+                return sec
+            if fld[dx][dy]:
+                fld[dx][dy] = 0
+            else:
+                q.pop()
+            q.appendleft((dx, dy))
+        else:
+            return sec
+        
+        if moving.get(sec, False):
+            if moving[sec] == 'D':
+                direction += 1
+            else:
+                direction -= 1
+            direction %= 4
+
+
+dir = [[0,1],[1,0],[0,-1],[-1,0]]
+input = sys.stdin.readline
+n = int(input())
+fld = [[0]*n for _ in range(n)]
+apple = int(input())
+for _ in range(apple):
+    u, v = map(int, input().split())
+    fld[u - 1][v - 1] = 1
+turn = int(input())
+moving = {}
+for _ in range(turn):
+    inp = input().rstrip().split()
+    moving[int(inp[0])] = inp[1]
+res = game()
+print(res)
