35a18eb3840d397f0d412a87a8679bd9eecd19ad
nastorond <nastorond@kakao.com>
2023-09-20 17:29:29 +0900

    update answer

--- Sep_23/23_9_20/sw_5248_sep_group.py
+++ Sep_23/23_9_20/sw_5248_sep_group.py
@@ -2,7 +2,46 @@ import sys
 from collections import deque
 sys.stdin = open('input.txt')
 
+
+def find_set(x):
+    if x == parent[x]:
+        return x
+    else:
+        return find_set(parent[x])
+
+
 # union-find
+def union(x, y):
+    x = find_set(x)
+    y = find_set(y)
+
+    if rank[x] >= rank[y]:
+        parent[y] = x
+    else:
+        parent[x] = y
+
+    if rank[x] == rank[y]:
+        rank[x] += 1
+
+
+T = int(input())
+for tc in range(1, T+1):
+    N, M = map(int, input().split())
+    edge = list(map(int, input().split()))
+
+    parent = list(range(N+1))
+    rank = [0] * (N+1)
+
+    for i in range(M):
+        u = edge[i*2]
+        v = edge[i*2+1]
+        union(u, v)
+
+    for i in range(1, N+1):
+        parent[i] = find_set(i)
+
+    print(f'#{tc} {len(set(parent)) - 1}')
+
 
 """
 # bfs 이용해서 연결되어 있는지 확인
