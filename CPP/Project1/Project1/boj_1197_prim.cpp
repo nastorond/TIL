3142dba0ee8604b26f19b0b0b447178f7a67a5d3
nastorond <nastorond@kakao.com>
2023-09-21 23:56:48 +0900

    update

---
+++ CPP/Project1/Project1/boj_1197_prim.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <queue>
+#include <functional>
+
+#define MAX 10005
+
+using namespace std;
+using ti3 = tuple<int, int, int>;
+
+int v, e;
+
+vector<pair<int, int>>adj[MAX];
+bool check[MAX];
+
+long long Prim() {
+	int cnt = 0;
+	long long tot = 0; // MST ºñ¿ë
+
+	priority_queue<ti3, vector<ti3>, greater<ti3>>heap;
+	for (auto next : adj[1]) {
+		heap.push({ next.first, 1, next.second });
+	}
+	check[1] = 1;
+
+	while (1) {
+		int cost, v1, v2;
+		tie(cost, v1, v2) = heap.top();
+		heap.pop();
+		if (check[v2])continue;
+
+		check[v2] = 1;
+		cnt++;
+		tot += cost;
+		if (cnt == v - 1) break;
+		for (auto next : adj[v2]) {
+			if (check[next.second])continue;
+			heap.push({ next.first, v1, next.second });
+		}
+	}
+
+	return tot;
+}
+
+int main() {
+	cin >> v >> e;
+	for (int i = 0; i < e; i++) {
+		int from, to, cost;
+		cin >> from >> to >> cost;
+		adj[from].push_back({ cost, to });
+		adj[to].push_back({ cost, from });
+	}
+	long long ans = Prim();
+
+	cout << ans;
+
+	return 0;
+}
--- CPP/Project1/Project1/source1.cpp
+++ CPP/Project1/Project1/source1.cpp
@@ -1,36 +1,53 @@
-#include<iostream>
-typedef unsigned long ULONG;
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
+typedef pair<int, pair<int, int>> T;
 
-typedef struct Matrix_2x2 {
-	ULONG f[2][2];
-}Matrix2x2;
-Maxtrix2x2 multiply(Matrix2x2 A, Matrix2x2 B) {
-	Matrix2x2 C;
-	C.f[0][0] = A.f[0][0] * B.f[0][0] + A[0][1] * B[1][0];
-	C.f[0][1] = A.f[0][0] * B.f[0][1] + A[0][1] * B[1][1];
-	C.f[1][0] = A.f[1][0] * B.f[0][0] + A[1][1] * B[1][0];
-	C.f[1][1] = A.f[1][0] * B.f[0][1] + A[1][1] * B[1][1];
+int V, E;
+vector<T> v;
+int parent[10000 + 1];
+int ans;
 
-	return C;
+int findParent(int x) {
+	if (parent[x] == x) return x;
+	return parent[x] = findParent(parent[x]);
 }
-Matrix2x2 Matrix_Power(Matrix2x2 A, int n) {
-	if (n > 1) {
-		A = Matrix_Power(A, n / 2);
-		A = multiply(A, A);
 
-		if (n & 1) {
-			Matrix2x2 F1 = { 1,1,1,0 };
-			A = multiply(A, F1);
-		}
+void unionParent(int a, int b) {
+	a = findParent(a);
+	b = findParent(b);
+	parent[b] = a;
+}
+
+int main(void) {
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+	cin >> V >> E;
+	for (int i = 0; i < E; i++) {
+		int a, b, c;
+		cin >> a >> b >> c;
+		v.push_back({ c, {a, b} });
 	}
-	return A;
-}
+	sort(v.begin(), v.end());
+	for (int i = 1; i <= V; i++)
+		parent[i] = i;
 
-int main() {
-	int n;
-	cin >> n;
-	Matrix2x2 F1 = { 1,1,1,0 }
-	F1 = Matrix_Power(F1, n);
-	cout << F1.f[0][1] << endl;
+	int cnt = 0;
+	for (int i = 0; i < v.size(); i++) {
+		T curEdge = v[i];
+		int cost = curEdge.first;
+		int now = curEdge.second.first;
+		int next = curEdge.second.second;
+
+		if (findParent(now) == findParent(next)) continue; // 사이클이면 pass
+
+		unionParent(now, next);
+		ans += cost;
+
+		if (++cnt == V - 1) break;
+	}
+	cout << ans << '\n';
+	return 0;
 }
