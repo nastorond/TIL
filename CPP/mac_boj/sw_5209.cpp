b33600e730162d4f7d6d0b1ba5d218ca99298414
nas <nastorond@kakao.com>
2023-09-24 22:02:35 +0900

    solved

--- CPP/mac_boj/sw_5209.cpp
+++ CPP/mac_boj/sw_5209.cpp
@@ -5,34 +5,39 @@ using namespace std;
 
 int res = 100000000;
 int n;
+int arr[15][15];
 
-void find_ans(int visited[], int now, int now_val, int arr[]){
+void find_ans(bool visited[], int now, int now_val){
     if (res < now_val) return ;
 
-    int sum_val = 0;
-    for (int i=0; i<n; i++){
-        sum_val += visited[i];
-    }
-    if(sum_val == n){
+    if(now == n){
         if (now_val < res) res = now_val;
         return ;
     }
 
     for(int i = 0; i<n; i++){
-        if (visited[i] == 0){
-            visited[i] = 1;
-            find_ans(visited, now+1, now_val, arr);
-            visited[i] = 0;
+        if (visited[i] == false){
+            visited[i] = true;
+            find_ans(visited, now+1, now_val + arr[now][i]);
+            visited[i] = false;
         }
     }
+}
+void init(){
+    for(int i=0;i<15;i++){
+        for(int j=0;j<15;j++){
+            arr[i][j] = 0;
+        }
+    }
+    res = 1e9;
 }
 
 int main(){
     int T;
     cin >> T;
-    for (int i=1; i<=T; i++){
+    for (int tc=1; tc<=T; tc++){
         cin >> n;
-
+        init();
         // 동적할당으로 입력
         // int** arr = new int*[n];
         // for(int i=0;i<n;i++){
@@ -42,15 +47,15 @@ int main(){
         //         cin >> arr[i][j];
         //     }
         // }
-        vector<vector<int>> arr(n);
+        // vector<vector<int>> arr(n);
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
-                int t;
-                cin >> t;
-                arr[i].push_back(t);
+                cin >> arr[i][j];
             }
         }
-        // vector<int> visited[n];
-        // find_ans(visited, 0, 0, arr);
+        bool visited[n];
+        fill(visited, visited+n, false);
+        find_ans(visited, 0, 0);
+        cout << "#" << tc << " " << res << "\n";
     }
 }
