ab38e4ad57cbeeb0ef1af4c6e97910fc1839dee9
nas <nastorond@kakao.com>
2023-10-04 20:22:52 +0900

    asd

--- CPP/imp_algo/fibo_cq.cpp
+++ CPP/imp_algo/fibo_cq.cpp
@@ -5,7 +5,8 @@ using namespace std;
 typedef struct Matrix_2x2{
     ULONG f[2][2];
 }Matrix2x2;
-Maxtrix2x2 multiply(Matrix2x2 A, Matrix2x2 B){
+
+Matrix2x2 multiply(Matrix2x2 A, Matrix2x2 B){
     Matrix2x2 C;
     C.f[0][0] = A.f[0][0]*B.f[0][0] + A[0][1]*B[1][0];
     C.f[0][1] = A.f[0][0]*B.f[0][1] + A[0][1]*B[1][1];
@@ -14,6 +15,7 @@ Maxtrix2x2 multiply(Matrix2x2 A, Matrix2x2 B){
 
     return C;
 }
+
 Matrix2x2 Matrix_Power(Matrix2x2 A, int n){
     if (n > 1){
         A = Matrix_Power(A, n/2);
@@ -30,7 +32,7 @@ Matrix2x2 Matrix_Power(Matrix2x2 A, int n){
 int main(){
     int n;
     cin >> n;
-    Matrix2x2 F1 = { 1,1,1,0 }
+    Matrix2x2 F1 = { 1,1,1,0 };
     F1 = Matrix_Power(F1, n);
     cout << F1.f[0][1] << endl;
 }
