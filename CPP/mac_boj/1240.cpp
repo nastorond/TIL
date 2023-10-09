ae0c8fbd4370697153a73bffc731739c6feee7b1
nas <nastorond@kakao.com>
2023-10-09 17:02:53 +0900

    update files

---
+++ CPP/mac_boj/1240.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+    int n, m;
+    cin >> n >> m;
+    
+
+    return 0;
+}
