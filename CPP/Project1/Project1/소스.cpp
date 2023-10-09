c11b9adffae91d1e929b995a990ef2ab21b1e763
nastorond <nastorond@kakao.com>
2023-10-10 03:06:54 +0900

    update

--- CPP/Project1/Project1/소스.cpp
+++ CPP/Project1/Project1/소스.cpp
@@ -1,16 +1,9 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
-
 using namespace std;
-
 int main() {
-	vector<int> res;
-	for (int i = 0; i < 3; i++) {
-		int a;
-		cin >> a;
-		res.push_back(a);
-	}
-	sort(res.begin(), res.end());
-	cout << res[1] << "\n";
+	long long a, b;
+	cin >> a;
+	cin >> b;
+	cout << a + b;
+	return 0;
 }
