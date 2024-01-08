14560ee1e8961c9143b8b0e8398c6971865f7b3a
nastorond <nastorond@kakao.com>
2024-01-08 22:53:37 +0900

    m

---
+++ CPP/boj/2579.cpp
@@ -0,0 +1,27 @@
+// dp 계단오르기
+#include <iostream>
+#define MAX_VAL 302
+
+using namespace std;
+
+int n, stairs[MAX_VAL], dp[MAX_VAL];
+
+int Solution () {
+    for (int i=1; i<=n; i++) {
+        
+    }
+
+    return dp[n];
+}
+
+int main () {
+    ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+
+    cin >> n;
+    stairs[0] = 0;
+    for (int i=1; i<=n; i++) {
+        cin >> stairs[i];
+    }
+
+    return 0;
+}
