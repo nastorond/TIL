47fcf3f205d1ebf64dd6233567ec3675494e8157
nastorond <nastorond@kakao.com>
2024-01-08 17:50:10 +0900

    update

--- CPP/mac_boj/14502.cpp
+++ CPP/mac_boj/14502.cpp
@@ -1,20 +1,28 @@
-// 연구소
+// 14502 연구소
 #include <iostream>
-#define MAX_VAL 9
+#include <vector>
 
 using namespace std;
 
 int n, m;
-int gp[MAX_VAL][MAX_VAL];
+int gp[9][9];
+int visited[9][9];
+vector<pair<int, int>> virus[11];
 
 int main () {
     ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
 
+    // input
     cin >> n >> m;
-
+    int cnt = 0;
     for (int i=0; i<n; i++) {
         for (int j=0; j<m; j++) {
             cin >> gp[i][j];
+            visited[i][j] = 0;
+            if (gp[i][j] == 2) {
+                virus->push_back(make_pair(i,j));
+                cnt++;
+            }
         }
     }
     
