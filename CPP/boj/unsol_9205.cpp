84ba9027905553095fbb4f917af9b0f4bf85c917
nastorond <nastorond@kakao.com>
2024-01-10 16:54:02 +0900

    add prob

---
+++ CPP/boj/unsol_9205.cpp
@@ -0,0 +1,18 @@
+// 맥주 마시면서 걸어가기
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+
+    int test_case;
+
+    cin >> test_case;
+    for (int tc=1; tc<=test_case; tc++) {
+        
+    }
+
+
+    return 0;
+}
