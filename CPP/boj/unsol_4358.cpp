86faeac222f6b4da0a850f4f0af01071f0bcc8c9
박수형 <nastorond@kakao.com>
2024-02-01 10:30:07 +0900

    update

--- CPP/boj/unsol_4358.cpp
+++ CPP/boj/unsol_4358.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <map>
+#include <string>
 #define endl "\n"
 
 using namespace std;
@@ -6,7 +8,10 @@ using namespace std;
 int main () {
     ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
 
-
+    string s;
+    while(cin >> s) {
+        cout << s << endl;
+    }    
 
     return 0;
 }
