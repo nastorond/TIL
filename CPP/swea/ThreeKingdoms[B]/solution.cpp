3e7a1cc6bc95f16f1931cbef244bf616610ee2bb
박수형 <nastorond@kakao.com>
2024-02-19 10:33:26 +0900

    update

---
+++ CPP/swea/ThreeKingdoms[B]/solution.cpp
@@ -0,0 +1,24 @@
+void init(int N, int mSoldier[25][25], char mMonarch[25][25][11])
+{
+
+}
+
+void destroy()
+{
+
+}
+
+int ally(char mMonarchA[11], char mMonarchB[11])
+{	
+	return -4;
+}
+
+int attack(char mMonarchA[11], char mMonarchB[11], char mGeneral[11])
+{
+	return -3;
+}
+
+int recruit(char mMonarch[11], int mNum, int mOption)
+{
+	return -1;
+}
