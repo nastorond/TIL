c976fa2e6b71d30cce6e5ffaf0135e885d293b36
박수형 <nastorond@kakao.com>
2024-03-06 21:40:13 +0900

    update

--- CPP/boj/unsol_2579.cpp
+++ CPP/boj/unsol_2579.cpp
@@ -7,22 +7,21 @@ using namespace std;
 int n, stairs[MAX_VAL], dp[MAX_VAL];
 
 int Solution () {
-    for (int i=1; i<=n; i++) {
+    for (int i=1; i<n-1; i++) {
         
     }
 
-    return dp[n];
+    return dp[n-1] + stairs[n-1];
 }
 
 int main () {
     ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
 
     cin >> n;
-    stairs[0] = 0;
-    dp[0] = 0;
-    for (int i=1; i<=n; i++) {
+    for (int i=0; i<n; i++) {
         cin >> stairs[i];
     }
+    dp[0] = 0;
 
     return 0;
 }
