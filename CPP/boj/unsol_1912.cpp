d70cce44392dbd54364fb9480a75424fc5cd540e
박수형 <nastorond@kakao.com>
2024-03-06 21:30:10 +0900

    update

--- CPP/boj/unsol_1912.cpp
+++ CPP/boj/unsol_1912.cpp
@@ -1,4 +1,4 @@
-// boj 연속합 S2
+// boj 연속합
 #include <iostream>
 #define endl "\n"
 #define MAX_VAL 100010
@@ -9,36 +9,15 @@ int main () {
     ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
 
     int n, dp[MAX_VAL], vals[MAX_VAL];
-    bool flg = true;
 
     // input
     cin >> n;
-    for (int i=0; i<n; i++) {
+    for (int i=1; i<=n; i++) {
         cin >> vals[i];
     }
 
     dp[0] = 0;
-    dp[1] = vals[0];
-    for (int i=2; i<=n; i++) {
-        if (flg) {
-            if (dp[i-1] + vals[i-1] > dp[i-1]) { 
-                dp[i] = dp[i-1] + vals[i-1];
-            }
-            else {
-                dp[i] = dp[i-1];
-                flg = !flg;
-            }
-        }
-        else {
-            if (dp[i-1] < vals[i-1]) {
-                dp[i] = vals[i-1];
-                flg = !flg;
-            }
-            else {
-                dp[i] = dp[i-1];
-            }
-        }
-    }
+    
 
     cout << dp[n] << endl;
 
