3c4fdb73b8a60ed5c377aabe2a2efba66a193ec0
박수형 <nastorond@kakao.com>
2024-03-07 22:26:49 +0900

    update idea

--- CPP/boj/unsol_2579.cpp
+++ CPP/boj/unsol_2579.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int n, stairs[MAX_VAL], dp[MAX_VAL];
 
 int Solution () {
-    for (int i=1; i<n-1; i++) {
+    for (int i=3; i<n-1; i++) {
         
     }
 
@@ -25,3 +25,9 @@ int main () {
 
     return 0;
 }
+
+// 3개를 연속해서 밟을 수 없음
+// 한번 건너 뛰게되면 다음 칸은 무조건 밟아야 함
+// i-1 + i-2, i-1 + i ?? 이건가
+// 그럼 dp[3] 부터 시작해야대는디
+// dp [0] = 0; dp[1] = stair[1], dp[2] = dp[1] + stair[2], dp[1]
