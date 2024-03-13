020fe7eebe94bbedb044f1ca0e91d34fa573cf90
박수형 <nastorond@kakao.com>
2024-03-13 17:45:44 +0900

    update segTree

--- CPP/boj/unsol_2042.cpp
+++ CPP/boj/unsol_2042.cpp
@@ -1,71 +1,63 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
 #include <math.h>
-#define MAX_LEN 1000030
+#define MAX_VAL 1000030
 
 using namespace std;
 
-int N, M, K;
-vector<long long> arr;
-vector<long long> tree;
+int n, m, k;
 
-long long init (int node, int start, int end) {
-    if (start == end) return tree[node] = arr[start];
+long long arr[MAX_VAL];
 
-    int mid = (start  + end) / 2;
-    return tree[node] = init(node * 2, start, mid) + init(node * 2 + 1, mid + 1, end);
-}
-
-long long sum(int node, int start, int end, int left, int right) {
-    if (left > end || right < start) return 0;
-    if (left <= start && end <= right) return tree[node];
+long long init(vector<long long> &segTree, int node, int start, int end) {
+    if (start == end) return segTree[node] = arr[start];
 
     int mid = (start + end) / 2;
-    return sum(node * 2, start, mid, left, right) + sum(node * 2 + 1, mid+1 ,end, left, right);
+
+    return segTree[node] = init(segTree, node*2, start, mid) + init(segTree, node*2+1, mid+1, end);
 }
 
-void update(int node, int start, int end, int index, long long diff) {
-    if (index < start || index > end) return ;
-    tree[node] = tree[node] + diff;
+long long sum(vector<long long> &segTree, int node, int left, int right, int start, int end) {
+    if (left > end || right < start) return 0;
+    if (left <= start && right >= end) return segTree[node];
+    
+    int mid = (start + end) / 2;
+
+    return sum(segTree, node*2, left, right, start, mid) + sum(segTree, node*2+1, left, right, mid+1, end);
+}
+
+void update(vector<long long> &segTree, int node, int start, int end, int idx, long long diff) {
+    if (idx < start || idx > end) return ;
+    segTree[node] += diff;
     if (start != end) {
         int mid = (start + end) / 2;
-        update(node * 2, start, mid, index, diff);
-        update(node * 2 + 1, mid + 1, end, index, diff);
+        update(segTree, node*2, start, mid, idx, diff);
+        update(segTree, node*2+1, mid+1, end, idx, diff);
     }
 }
 
 int main() {
     ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
-
-    int a, b, h, tree_size;
-    long long c;
-
-    cin >> N >> M >> K;
-
-    h = (int)ceil(log2(N));
-    tree_size = (1 << (h + 1));
-    for (int i=0; i<tree_size; i++) {
-        tree.push_back(0);
+    cin >> n >> m >> k;
+    for (int i=0; i<n; i++){
+        cin >> arr[i];
     }
+    int h = ceil(log2(n));
+    int treeSize = 1 << (h+1);
+    vector<long long> segTree(treeSize);
 
-    for (int i=0; i<N; i++) {
-        cin >> c;
-        arr.push_back(c);
-    }
-
-    init(1, 0, N-1);
-
-    for (int i=0; i<M; i++) {
+    init(segTree, 1, 0, n-1);
+    for (int i=0; i < m + k; i++) {
+        int a, b;
+        long long c;
         cin >> a >> b >> c;
         if (a == 1) {
-            long long diff = arr[b] - c;
-            arr[b] = c;
-            update(1, 1, tree_size, b, diff);
+            update(segTree, 1, 0, n-1, b-1, c-arr[b-1]);
+            arr[b-1] = c;
+        } else {
+            cout << sum(segTree, 1, b-1, c-1, 0, n-1) << "\n";
         }
-        else {
-            int tmp = (int)c;
-            cout << sum(1, 1, tree_size, b, (int)c) << "\n";
-        }        
     }
+
+    return 0;
 }
