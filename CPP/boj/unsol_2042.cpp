f1e78e7934c53c5ba549712145743e5d0d34ce17
박수형 <nastorond@kakao.com>
2024-03-13 17:19:18 +0900

    뭐야 이거

--- CPP/boj/unsol_2042.cpp
+++ CPP/boj/unsol_2042.cpp
@@ -3,35 +3,69 @@
 #include <vector>
 #include <math.h>
 #define MAX_LEN 1000030
-typedef long long ll;
 
 using namespace std;
 
 int N, M, K;
-vector<ll> arr;
-vector<ll> tree;
+vector<long long> arr;
+vector<long long> tree;
 
-int init (int node, int start, int end) {
+long long init (int node, int start, int end) {
     if (start == end) return tree[node] = arr[start];
 
     int mid = (start  + end) / 2;
     return tree[node] = init(node * 2, start, mid) + init(node * 2 + 1, mid + 1, end);
 }
 
+long long sum(int node, int start, int end, int left, int right) {
+    if (left > end || right < start) return 0;
+    if (left <= start && end <= right) return tree[node];
+
+    int mid = (start + end) / 2;
+    return sum(node * 2, start, mid, left, right) + sum(node * 2 + 1, mid+1 ,end, left, right);
+}
+
+void update(int node, int start, int end, int index, long long diff) {
+    if (index < start || index > end) return ;
+    tree[node] = tree[node] + diff;
+    if (start != end) {
+        int mid = (start + end) / 2;
+        update(node * 2, start, mid, index, diff);
+        update(node * 2 + 1, mid + 1, end, index, diff);
+    }
+}
+
 int main() {
     ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
 
     int a, b, h, tree_size;
-    ll c;
+    long long c;
 
     cin >> N >> M >> K;
 
     h = (int)ceil(log2(N));
     tree_size = (1 << (h + 1));
-    fill(tree.begin(), tree.begin() + tree_size, 0);
+    for (int i=0; i<tree_size; i++) {
+        tree.push_back(0);
+    }
 
-    for (int i=0; i<M; i++) {
+    for (int i=0; i<N; i++) {
         cin >> c;
         arr.push_back(c);
+    }
+
+    init(1, 0, N-1);
+
+    for (int i=0; i<M; i++) {
+        cin >> a >> b >> c;
+        if (a == 1) {
+            long long diff = arr[b] - c;
+            arr[b] = c;
+            update(1, 1, tree_size, b, diff);
+        }
+        else {
+            int tmp = (int)c;
+            cout << sum(1, 1, tree_size, b, (int)c) << "\n";
+        }        
     }
 }
