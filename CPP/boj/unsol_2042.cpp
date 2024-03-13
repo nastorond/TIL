a3e41deb0564c949cd088e9e368b56ce404ba1d0
박수형 <nastorond@kakao.com>
2024-03-13 12:25:45 +0900

    UPDATE"

--- CPP/boj/unsol_2042.cpp
+++ CPP/boj/unsol_2042.cpp
@@ -3,22 +3,31 @@
 #include <vector>
 #include <math.h>
 #define MAX_LEN 1000030
+typedef long long ll;
 
 using namespace std;
 
 int N, M, K;
-vector<long long> arr;
-vector<long long> tree;
+vector<ll> arr;
+vector<ll> tree;
+
+int init (int node, int start, int end) {
+    if (start == end) return tree[node] = arr[start];
+
+    int mid = (start  + end) / 2;
+    return tree[node] = init(node * 2, start, mid) + init(node * 2 + 1, mid + 1, end);
+}
 
 int main() {
     ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
 
-    int a, b, tree_size;
-    long long c;
+    int a, b, h, tree_size;
+    ll c;
 
     cin >> N >> M >> K;
 
-    tree_size = (int)ceil(log2(N));
+    h = (int)ceil(log2(N));
+    tree_size = (1 << (h + 1));
     fill(tree.begin(), tree.begin() + tree_size, 0);
 
     for (int i=0; i<M; i++) {
