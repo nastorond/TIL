6fd5957760be3b3cb787eb664f9d3bab9f16c44d
박수형 <nastorond@kakao.com>
2024-03-22 10:01:51 +0900

    update

--- CPP/boj/unsol_3197.cpp
+++ CPP/boj/unsol_3197.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <queue>
-#include <string>
+#include <tuple>
 #define MAX_LEN 1501
 
 using namespace std;
@@ -12,29 +12,31 @@ int map[MAX_LEN][MAX_LEN];
 int visited[MAX_LEN][MAX_LEN];
 
 int dijkstra() {
-    priority_queue<pair<int, int>> pq;
-    pq.push(make_pair(x, y));
+    priority_queue<tuple<int, int, int>> pq;
+    pq.push(make_tuple(0, x, y));
     visited[x][y] = 0;
     while (!pq.empty()) {
-        x = pq.top().first;
-        y = pq.top().second;
+        x = get<1>(pq.top());
+        y = get<2>(pq.top());
+        int dist = -get<0>(pq.top());
         pq.pop();
-        if (map[x][y] == 2) {
-            return visited[x][y];
-        }
         for (int i=0; i<4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
             if (0<=nx && nx<n && 0<=ny && ny<m) {
-                if (map[nx][ny] == 1 && visited[x][y] + 1 < visited[nx][ny]) {
-                    visited[nx][ny] += 1;
+                if (map[nx][ny] == 2) {
+                    return visited[x][y];
+                } else if (map[nx][ny] == 1 && dist + 1 < visited[nx][ny]) {
+                    visited[nx][ny] = dist + 1;
+                    pq.push(make_tuple(-(dist + 1), nx, ny));
+                } else {
+                    visited[nx][ny] = min(visited[x][y], visited[nx][ny]);
+                    pq.push(make_tuple(dist, nx, ny));
                 }
-                pq.push(make_pair(nx, ny));
-                visited[nx][ny] = min(visited[x][y], visited[nx][ny]);
             }
         }
     }
-    return 0;
+    return 10;
 }
 
 int main() {
@@ -43,21 +45,21 @@ int main() {
     cin >> n >> m;
 
     string inp;
+    char condition1 = '.';
+    char condition2 = 'X';
+    char condition3 = 'L';
     for (int i=0; i<n; i++){
         cin >> inp;
         int cnt=0;
-        cout << "test" << "\n";
         for (char c: inp) {
-            if (c == ".") {
+            if (c == condition1) {
                 map[i][cnt] = 0;
-            } else if (c == 'X') {
+            } else if (c == condition2) {
                 map[i][cnt] = 1;
-            } else if (c == 'L') {
+            } else if (c == condition3) {
                 map[i][cnt] = 2;
                 x = i;
                 y = cnt;
-            } else {
-                cout << "check" << "\n";
             }
             visited[i][cnt] = 1e9;
             cnt++;
