f3b009fe6e2e73bdc0724eb583ef77f1c159c869
박수형 <nastorond@kakao.com>
2024-03-26 22:20:52 +0900

    update

--- CPP/boj/3197.cpp
+++ CPP/boj/3197.cpp
@@ -1,3 +1,4 @@
+// 효율적인 BFS 완전 탐색
 #include <iostream>
 #include <queue>
 #include <vector>
@@ -25,19 +26,37 @@ queue<pair<int, int>> bfs(queue<pair<int, int>> &tmp) {
             int ny = y + dy[i];
 
             if (0<=nx && nx<n && 0<=ny && ny<m && !visited[nx][ny]) {
-                switch (map[nx][ny])
-                {
-                case 0:
+                visited[nx][ny] = true;
+
+                if (map[nx][ny] == 0) {
                     tmp.push(make_pair(nx, ny));
-                    visited[nx][ny] = true;
-                    break;
-                case 1:
+                } else if (map[nx][ny] == 1) {
                     res.push(make_pair(nx, ny));
-                    visited[nx][ny] = true;
-                    break;
-                case 2:
+                } else {
                     flg=true;
                     return res;
+                }
+            }
+        }
+    }
+    return res;
+}
+
+queue<pair<int, int>> bfs2(queue<pair<int, int>> &tmp) {
+    queue<pair<int,int>> res;
+    while (!tmp.empty()) {
+        x = tmp.front().first;
+        y = tmp.front().second;
+        tmp.pop();
+
+        for (int i=0; i<4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (0<=nx && nx<n && 0<=ny && ny<m) {
+                if (map[nx][ny] == 1) {
+                    res.push(make_pair(nx, ny));
+                    map[nx][ny] = 0;
                 }
             }
         }
@@ -51,6 +70,8 @@ int main() {
     cin >> n >> m;
 
     vector<pair<int, int>> start_point;
+    queue<pair<int,int>> tmp;
+
     string inp;
     char condition1 = '.';
     char condition2 = 'X';
@@ -61,10 +82,12 @@ int main() {
         for (char c: inp) {
             if (c == condition1) {
                 map[i][cnt] = 0;
+                tmp.push(make_pair(i, cnt));
             } else if (c == condition2) {
                 map[i][cnt] = 1;
             } else if (c == condition3) {
                 map[i][cnt] = 2;
+                tmp.push(make_pair(i, cnt));
                 x = i;
                 y = cnt;
             }
@@ -74,17 +97,20 @@ int main() {
     }
 
     int cnt=0;
-    queue<pair<int,int>> tmp;
     visited[x][y] = true;
-    tmp.push(make_pair(x, y));
+    queue<pair<int, int>> tmp2;
+    tmp2.push(make_pair(x, y));
 
-    while(true) {
-        tmp = bfs(tmp);
-        if (!flg) cnt++;
+    while(!flg) {
+        tmp2 = bfs(tmp2);
+        if (!flg) {
+            tmp = bfs2(tmp);
+            cnt++;
+        }
         else break;
     }
 
-    cout << cnt/2 + cnt%2 << "\n";
+    cout << cnt << "\n";
 
     return 0;
 }
