d02b6c782c8c7889fd310bd31cda16b28d9496cc
박수형 <nastorond@kakao.com>
2024-03-27 19:31:13 +0900

    update

---
+++ CPP/boj/unsol_11053.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#define MAX_VAL 1002
+
+using namespace std;
+
+int n;
+int arr[MAX_VAL];
+
+int main () {
+    ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+
+    cin >> n;
+    for (int i=0; i<n; i++) {
+        cin >> arr[i];
+    }
+
+    return 0;
+}
