82986a68552816e4f257957b3d41a70830d39153
박수형 <nastorond@kakao.com>
2024-04-10 15:03:06 +0900

    UPdate"

--- CPP/boj/unsol_2206.cpp
+++ CPP/boj/unsol_2206.cpp
@@ -7,7 +7,6 @@
 using namespace std;
 
 int n, m, maps[MAX_LEN][MAX_LEN], visited[MAX_LEN][MAX_LEN][2];
-vector<pair<int, int>> walls;
 int dx[] = {1, 0, -1, 0};
 int dy[] = {0, -1, 0, 1};
 
@@ -37,11 +36,16 @@ int bfs() {
         for (int i=0; i<4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
-            if (0<=nx && nx<n && 0<=ny && ny<m) {
-                if (visited[nx][ny][0]==0 && maps[nx][ny] == 0) {
+            if (0<=nx && nx<n && 0<=ny && ny<m && visited[nx][ny][0]==0) {
+                if (maps[nx][ny]==0) {
                     visited[nx][ny][0] = visited[x][y][0] + 1;
                     q.push(make_pair(nx, ny));
+                } else if (visited[x][y][1] == 0 && maps[nx][ny] == 1) {
+                    visited[nx][ny][0] = visited[x][y][0] + 1;
+                    visited[nx][ny][1] = 1;
+                    q.push(make_pair(nx, ny));
                 }
+                if (visited[x][y][1] == 1) visited[nx][ny][1] = 1;
             }
         }
     }
@@ -60,29 +64,13 @@ int main () {
 
         for (char j:tmp) {
             maps[i][cnt] = j-'0';
-            if(j=='1') walls.push_back(make_pair(i, cnt));
+            // if(j=='1') walls.push_back(make_pair(i, cnt));
             cnt++; 
         }
     }
 
 
     int res = bfs();
-
-    // cout << res << "\n";
-
-    // for (int i=0; i<n; i++) {
-    //     for (int j=0; j<m; j++) {
-    //         cout << maps[i][j] << " ";
-    //     }
-    //     cout << "\n";
-    // }
-    // cout << "\n";
-
-    for (auto it: walls) {
-        maps[it.first][it.second] = 0;
-        res = min(res, bfs());
-        maps[it.first][it.second] = 1;
-    }
 
     if (res >= 1e9) cout << -1 << "\n";
     else cout << res << "\n";
