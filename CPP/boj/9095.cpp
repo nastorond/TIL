81dace5392de50c1ebc7a630fbfde382b6e7b2c8
박수형 <nastorond@kakao.com>
2024-05-09 21:20:10 +0900

    update

---
+++ CPP/boj/9095.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int dp[12] = {};
+int n, target_num;
+
+void dfs(int cur_val) {
+    if (cur_val > target_num) return ;
+    if (cur_val == target_num) {
+        dp[cur_val]++;
+        return ;
+    }
+    dfs(cur_val+1);
+    dfs(cur_val+2);
+    dfs(cur_val+3);
+}
+
+int main () {
+    ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+
+    cin >> n;
+    for (int i=0; i<n; i++) {
+        cin >> target_num;
+        dfs(0);
+        cout << dp[target_num] << "\n";
+    }
+
+    return 0;
+}
