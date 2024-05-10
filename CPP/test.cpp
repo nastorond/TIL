a2437eb546a2fd71b547fab08ab304dccd2518da
박수형 <nastorond@kakao.com>
2024-05-10 16:15:01 +0900

    c++은 할당 시간에 차이가 없다

--- CPP/test.cpp
+++ CPP/test.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <time.h>
 #define endl "\n"
 
 using namespace std;
@@ -18,6 +19,19 @@ int main () {
     cout << "b" << " " << b << " size " << sizeof(b) << endl;
     cout << "c" << " " << c << " size " << sizeof(c) << endl;
     cout << "d" << " " << d << " size " << sizeof(d) << endl;
+
+    time_t statrTime = 0, check1=0, check2=0;
+
+    statrTime=clock();
+    for (int i=0; i<100; i++) int arr[100000][2];
+    
+    check1 = clock() - statrTime;
+
+    statrTime=clock();
+    int arr2[2][100000];
+    check2=clock() - statrTime;
+    cout << check1 << endl;
+    cout << check2 << endl;
 
     return 0;
 }
