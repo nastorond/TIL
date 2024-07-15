84858f1133425b82689738c67439524bb73a4e46
박수형 <nastorond@kakao.com>
2024-07-15 21:38:51 +0900

    update prob

---
+++ CPP/swea/samsung_summer_dx/solution.cpp
@@ -0,0 +1,24 @@
+#define MAXL	5
+#define MAXF	10
+
+using namespace std;
+
+void init(int N)
+{
+	
+}
+
+void add(int id, int F, int ids[MAXF])
+{
+
+}
+
+void del(int id1, int id2)
+{
+
+}
+
+int recommend(int id, int list[MAXL])
+{
+	return 0;
+}
