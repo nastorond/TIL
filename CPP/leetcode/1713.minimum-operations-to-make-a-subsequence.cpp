1427523a0b1a0cf59a3e503b6287799c672cd591
박수형 <nastorond@kakao.com>
2024-08-31 16:24:14 +0900

    update answer Runtime: 214 ms, faster than 70.20% of C++ online submissions for Minimum Operations to Make a Subsequence. Memory Usage: 113.7 MB, less than 92.05% of C++ online submissions for Minimum Operations to Make a Subsequence.

--- CPP/leetcode/1713.minimum-operations-to-make-a-subsequence.cpp
+++ CPP/leetcode/1713.minimum-operations-to-make-a-subsequence.cpp
@@ -8,54 +8,25 @@
 class Solution {
 public:
     int minOperations(vector<int>& target, vector<int>& arr) {
-        int lim = target.size(), n=arr.size(), answer=0;
-        vector<pair<int, set<int>>> data;
-        vector<pair<int, int>> nums;
+        unordered_map<int, int> hashing;
+        vector<int> tmp;
+        int lim=target.size();
         
-        pair<int, int> tmp;
-        for (int i=0; i<arr.size(); i++) {
-            tmp.first = arr[i];
-            tmp.second = i;
-            nums.push_back(tmp);
-        }
-
-        sort(nums.begin(), nums.end());
-
-        for (int i=0; i<lim; i++) {
-            int idx = lower_bound(nums.begin(), nums.end(), make_pair(target[i], 0)) - nums.begin();
-
-            set<int> tmpSet; 
-            tmpSet; // insert basic val
-
-            while (idx < nums.size() && nums[idx].first == target[i]) {
-                tmpSet.insert(nums[idx].second);
-                idx++;
-            }
-
-            data.push_back(make_pair(target[i], tmpSet));
-        }
-
-        int idx = 0;
-        for (int i=0; i<lim; i++) {
+        for (int i=0; i<lim; i++) hashing[target[i]] = i;
+        
+        for (int i : arr) {
+            if (hashing.find(i) == hashing.end()) continue;
             
-            if (data[i].second.empty()) {
-                answer++;
+            auto it = lower_bound(tmp.begin(), tmp.end(), hashing[i]);
+            
+            if (it == tmp.end()) {
+                tmp.push_back(hashing[i]);
             } else {
-                auto tmp = data[i].second.begin();
-                while (1) {
-                    if (idx < *tmp) {
-                        idx = *tmp;
-                        break;
-                    }
-                    tmp++;
-                    if (tmp == data[i].second.end()) {
-                        answer++;
-                        break;
-                    }
-                }
+                *it = hashing[i];
             }
         }
-        return answer;
+        
+        return lim - tmp.size();
     }
 };
 // @lc code=end
