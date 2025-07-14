89507eabf84fe43c547ece61490998aa9771af08
nastorond <86664097+nastorond@users.noreply.github.com>
2025-07-14 21:21:45 +0900

    Title: 수 등장 횟수, Time: 67ms, Memory: 2MB, Status: Passed - Codetree

--- 250714/수 등장 횟수/number-frequency.cpp
+++ 250714/수 등장 횟수/number-frequency.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <unordered_map>
 
 using namespace std;
 
@@ -7,22 +8,42 @@ const int MAX_N = 100001;
 int n, m;
 int arr[MAX_N];
 int nums[MAX_N];
-int ck[MAX_N] = {0, };
+unordered_map<int, int> mp;
 
 int main() {
     cin >> n >> m;
 
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
-        ck[arr[i]-1]++;
     }
 
     for (int i = 0; i < m; i++) {
         cin >> nums[i];
-        cout << ck[nums[i]-1] << " ";
     }
 
     // Please write your code here.
+    int k;
+    for (int i=0; i<n; i++)
+    {
+        k = arr[i];
+        auto it = mp.find(k);
+        if (it != mp.end())
+        {
+            it->second++;
+        }
+        else 
+        {
+            mp.insert({k, 1});
+        }
+    }
+
+    for (int i=0; i<m; i++)
+    {
+        k = nums[i];
+        auto it = mp.find(k);
+        if (it != mp.end()) cout << it->second << " ";
+        else cout << "0 ";
+    }
 
     return 0;
 }
