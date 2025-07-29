70305c84b39aa90537c39aae94c1866ed09293c3
nastorond <86664097+nastorond@users.noreply.github.com>
2025-07-29 21:52:37 +0900

    Title: 트리의 부모 노드, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250729/트리의 부모 노드/parent-node-of-the-tree.cpp
+++ 250729/트리의 부모 노드/parent-node-of-the-tree.cpp
@@ -4,28 +4,28 @@
 using namespace std;
 
 int n;
-int from[100000], to[100000];
+// int from[100000], to[100000];
 unordered_map<int, int> tree;
+int arr[100000] = {0, };
 
 int main() {
     cin >> n;
-    for (int i = 0; i < n - 1; i++) {
-        cin >> from[i] >> to[i];
-    }
+    // for (int i = 0; i < n - 1; i++) {
+    //     cin >> from[i] >> to[i];
+    // }
 
     // Please write your code here.
+    int from, to;
     for (int i=0; i<n-1;i++)
     {
-        tree.insert({to[i], from[i]});
+        cin >> from >> to;
+        arr[to] = from;
     }
 
     for (int i=2; i<n+1; i++)
     {
-        auto tmp = tree.find(i);
-        if (tmp != tree.end())
-        {
-            cout << tmp->second << "\n";
-        }
+        if (arr[i] == 0) continue;
+        cout << arr[i] << "\n";
     }
 
     return 0;
