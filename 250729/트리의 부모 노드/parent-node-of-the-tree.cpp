5279dd8cdaa0ab000f925bb5eddd33d72d5b8eb0
nastorond <86664097+nastorond@users.noreply.github.com>
2025-07-29 22:00:48 +0900

    Title: 트리의 부모 노드, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250729/트리의 부모 노드/parent-node-of-the-tree.cpp
+++ 250729/트리의 부모 노드/parent-node-of-the-tree.cpp
@@ -1,31 +1,57 @@
 #include <iostream>
 #include <unordered_map>
+#include <vector>
 
 using namespace std;
 
 int n;
-// int from[100000], to[100000];
-unordered_map<int, int> tree;
-int arr[100000] = {0, };
+int from[100000], to[100000];
+unordered_map<int, vector<int>> tree;
+int roots[100001] = {0, };
+
+void findingRoot(int cur)
+{
+    auto tmp = tree.find(cur);
+    if (tmp == tree.end()) return ;
+
+    for (int chd : tree[cur])
+    {
+        if (roots[chd] != 0) continue;
+        roots[chd] = cur;
+        findingRoot(chd);
+    }
+}
 
 int main() {
     cin >> n;
-    // for (int i = 0; i < n - 1; i++) {
-    //     cin >> from[i] >> to[i];
-    // }
-
-    // Please write your code here.
-    int from, to;
-    for (int i=0; i<n-1;i++)
-    {
-        cin >> from >> to;
-        arr[to] = from;
+    for (int i = 0; i < n - 1; i++) {
+        cin >> from[i] >> to[i];
     }
 
+    // Please write your code here.
+    for (int i=0; i<n-1;i++)
+    {
+        auto tmp = tree.find(from[i]);
+        if (tmp != tree.end())
+        {
+            tree[from[i]].push_back(to[i]);
+        }
+        else
+        {
+            vector<int> tV;
+            tV.push_back(to[i]);
+            tree[from[i]] = tV;
+        }
+    }
+
+    findingRoot(1);
+    
     for (int i=2; i<n+1; i++)
     {
-        if (arr[i] == 0) continue;
-        cout << arr[i] << "\n";
+        if (roots[i] != 0)
+        {
+            cout << roots[i] << "\n";
+        }
     }
 
     return 0;
