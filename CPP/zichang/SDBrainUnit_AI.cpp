5346c97ddfeafcd999eff4b8554e5c2a0b5386f5
박수형 <nastorond@kakao.com>
2026-04-05 11:45:18 +0900

    update code

--- CPP/zichang/SDBrainUnit_AI.cpp
+++ CPP/zichang/SDBrainUnit_AI.cpp
@@ -1,74 +1,119 @@
-//! 위상정렬 기반의 코드
 #include <iostream>
 #include <vector>
 #include <queue>
 
 using namespace std;
 
+//! 부품 총 개수, 의존성 관계 수
 int N, M;
-// current -> {next, k}
-vector<vector<pair<int,int>>> adj;
-vector<int> indegree;
-// dp[i][j] = i를 만들 때 필요한 기본 부품 j 개수
-vector<vector<int>> dp;
 
-void init() {
+//! 관계 그래프
+//! Y -> {X, K}
+//! Y를 K개 사용해서 X를 만듦
+std::vector<std::vector<std::pair<int, int>>> tree;
+
+//! 진입 차수: 해당 부품을 만들기 위해 필요한 하위 부품 수
+std::vector<int> indegree;
+
+//! 나가는 차수: 해당 부품이 상위 부품 제작에 사용되는 횟수
+std::vector<int> outdegree;
+
+//! basic 부품 여부
+std::vector<bool> isBasic;
+
+//! dp[u][b] = u를 1개 만들 때 기본 부품 b가 필요한 개수
+std::vector<std::vector<int>> dp;
+
+void init()
+{
     cin >> N >> M;
-    adj.assign(N + 1, {});
-    indegree.assign(N + 1, 0);
-    dp.assign(N + 1, vector<int>(N + 1, 0));
 
-    for (int i = 0; i < M; ++i) {
-        int X, Y, K;
+    tree.resize(N + 1, std::vector<std::pair<int, int>>());
+    indegree.resize(N + 1, 0);
+    outdegree.resize(N + 1, 0);
+    isBasic.resize(N + 1, false);
+    dp.resize(N + 1, std::vector<int>(N + 1, 0));
+
+    int X, Y, K;
+    for (int i = 0; i < M; ++i)
+    {
         cin >> X >> Y >> K;
-        adj[Y].push_back({X, K});   // Y -> X
-        indegree[X]++;              // X로 들어오는 간선 +1
+
+        //! Y -> X
+        tree[Y].push_back(std::make_pair(X, K));
+
+        indegree[X]++;
+        outdegree[Y]++;
     }
 }
 
-void solution() {
+void solution()
+{
     queue<int> q;
 
-    // 기본 부품: 진입 차수 0
-    for (int i = 1; i <= N; ++i) {
-        if (indegree[i] == 0) {
+    //! 기본 부품 찾기: indegree == 0
+    for (int i = 1; i <= N; ++i)
+    {
+        if (indegree[i] == 0)
+        {
             q.push(i);
-            dp[i][i] = 1;   // 기본 부품 자기 자신 1개
+            isBasic[i] = true;
+            dp[i][i] = 1; //! 기본 부품 자기 자신 1개
         }
     }
 
-    while (!q.empty()) {
+    //! 위상정렬 + DP
+    while (!q.empty())
+    {
         int cur = q.front();
         q.pop();
 
-        for (const auto &nx : adj[cur]) {
-            int next = nx.first;
-            int k    = nx.second;
+        for (const std::pair<int, int>& next : tree[cur])
+        {
+            int nextNode = next.first;
+            int needCount = next.second;
 
-            // cur을 k개 써서 next 1개 만듦
-            for (int j = 1; j <= N; ++j) {
-                dp[next][j] += dp[cur][j] * k;
+            //! cur 부품 정보로 nextNode의 기본 부품 개수 누적
+            for (int basic = 1; basic <= N; ++basic)
+            {
+                dp[nextNode][basic] += dp[cur][basic] * needCount;
             }
 
-            if (--indegree[next] == 0) {
-                q.push(next);
+            indegree[nextNode]--;
+
+            if (indegree[nextNode] == 0)
+            {
+                q.push(nextNode);
             }
         }
     }
 
-    // 완제품 N에 대한 기본 부품 개수 출력
-    for (int j = 1; j <= N; ++j) {
-        if (dp[N][j] > 0) {
-            cout << j << " " << dp[N][j] << "\n";
+    //! 완제품 출력: outdegree == 0
+    //! 완제품이 여러 개일 수 있으므로 모두 출력
+    for (int product = 1; product <= N; ++product)
+    {
+        if (outdegree[product] == 0)
+        {
+            cout << "product " << product << "\n";
+
+            for (int basic = 1; basic <= N; ++basic)
+            {
+                if (isBasic[basic] && dp[product][basic] > 0)
+                {
+                    cout << basic << " " << dp[product][basic] << "\n";
+                }
+            }
         }
     }
 }
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     init();
     solution();
+
     return 0;
 }
